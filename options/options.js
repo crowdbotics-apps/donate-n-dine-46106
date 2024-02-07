268e9765b5416cbe80b327d5be63862dc5359c20
Crowdbotics <team@crowdbotics.com>
2024-02-07 20:18:44 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "donate-n-dine-46106",
+  url: "https://donate-n-dine-46106.botics.co",
+  api: "https://donate-n-dine-46106.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
